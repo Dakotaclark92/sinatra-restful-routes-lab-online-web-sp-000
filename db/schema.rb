8d96945e205f12f9cbe0207c9b4405fe3ba558c4
Dakota Clark <dakotaclark92@yahoo.com>
2019-09-05 01:29:10 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,9 +1,15 @@
-ActiveRecord::Schema.define(version: 20190828223625) do
+ActiveRecord::Schema.define(version: 20190829020519) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.string "ingredients"
     t.string "cook_time"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
+    t.string "cooktime"
   end
 
 end
